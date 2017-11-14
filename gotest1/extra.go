3d3b09803d4b6f8fc6b4deb52a45d5f566eff062
drell47 <dellison47@gmail.com>
2017-11-14 13:20:44 -0800

    first commit to gotest1 directory

---
+++ gotest1/extra.go
@@ -0,0 +1,6 @@
+package main
+
+func junk(x int) int {
+	y := x + 3
+	return y
+}
---
+++ gotest1/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world, testing 1, 2, 3.\n")
+	fmt.Println("This is a test of a testing")
+	z := 5
+	zp := junk(z)
+	fmt.Println(zp)
+}
