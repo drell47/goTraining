cba515b272554e3e0f676c54c5c1d108b0ae08db
drell47 <dellison47@gmail.com>
2017-11-14 13:31:41 -0800

    Update to gotest1/hello.go for comment

--- gotest1/hello.go
+++ gotest1/hello.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Printf("Hello, world, testing 1, 2, 3.\n")
 	fmt.Println("This is a test of a testing")
+	// set value for z
 	z := 5
 	zp := junk(z)
 	fmt.Println(zp)
